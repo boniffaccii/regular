8f21d52806698ac479a54b353d44ef833e3af75a
Bogdan <b.vaschenko@gmail.com>
2022-04-01 00:54:50 +0300

    3 ex

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ lastname,firstname,surname,organization,position,phone,email
 Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
 объединить все дублирующиеся записи о человеке в одну."""
 
-from pprint import pprint
+# from pprint import pprint
 import re
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
@@ -21,7 +21,6 @@ with open("phonebook_raw.csv") as f:
 # pprint(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
 f_contacts_list = []
 great_cl = []
 for row in contacts_list:
@@ -41,10 +40,23 @@ for row in contacts_list:
     else:
         phone = re.sub(pattern, r'+7(\2)\3-\4-\5', row[5])
         row[5] = phone
-
-    print(row)
     f_contacts_list.append(row)
 
+for row1 in f_contacts_list:
+    for row2 in f_contacts_list:
+        if row1[0] == row2[0] and row1[1] == row2[1]:
+            if row1 != row2:
+                d = 0
+                result = []
+                for d in range(len(row1)):
+                    if row1[d] == '':
+                        result.append(row2[d])
+                    else:
+                        result.append(row1[d])
+                f_contacts_list.remove(row2)
+                f_contacts_list.append(result)
+                f_contacts_list.remove(row1)
+                break
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
